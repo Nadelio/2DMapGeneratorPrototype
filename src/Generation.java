1c983fa75ffbd76feaf31582e2be002d3eae4530
Nadelio <carefulsniffle@gmail.com>
2024-06-19 07:39:58 -0500

    Tweaked some values Various fixes Changes to how map.png is colored

--- src/Generation.java
+++ src/Generation.java
@@ -11,20 +11,28 @@ import javax.imageio.ImageIO;
 public class Generation {
     public static String generateTerrainMap(int seed, int[] bounds){
         PerlinNoiseGenerator png = PerlinNoiseGenerator.newBuilder().setSeed(seed).setInterpolation(Interpolation.COSINE).build();
+        PerlinNoiseGenerator png1 = PerlinNoiseGenerator.newBuilder().setSeed(Main.generateSeed()).setInterpolation(Interpolation.COSINE).build();
+        PerlinNoiseGenerator[] pngs = {png, png1};
         float[][] map = new float[bounds[0]][bounds[1]];
-        float scale = 0.2f;
-        float amp = 0.6f;
-        float freq = 0.3f;
-        
+        float islandSize = 0.2f;
+        float amp = 0.7f;
+        float freq = 0.2f;
+        float groundThreshold = 0.2f;
+        float waterThreshold = 0.4f;
+        float deepWaterThreshold = 1.0f;
+
+
         for(int  x = 0; x < map.length; x++){
             for(int y = 0; y < map[x].length; y++){
                 
-                float xPos = (float) (x * scale);
-                float yPos = (float) (y * scale);
+                float xPos = (float) (x * islandSize);
+                float yPos = (float) (y * islandSize);
 
                 float normalize = 0.0f;
-                map[x][y] += amp * png.evaluateNoise(xPos * freq + seed, yPos * freq + seed);
-                normalize += amp;
+                for(PerlinNoiseGenerator pn : pngs){                    
+                    map[x][y] += amp * pn.evaluateNoise(xPos * freq + seed, yPos * freq + seed);
+                    normalize += amp;
+                }
 
                 map[x][y] /= normalize;
             }
@@ -34,33 +42,46 @@ public class Generation {
 
         for(int x = 0; x < map.length; x++){
             for(int y = 0; y < map[x].length; y++){
-                if(map[x][y] < 0.1f){
-                    finalMapAsChar[x][y] = '~';
-                } else if(map[x][y] < 0.5f && map[x][y] > 0.1f){
+                if(map[x][y] <= groundThreshold){
                     finalMapAsChar[x][y] = 'G';
-                } else if(map[x][y] <= 1.0f){
-                    finalMapAsChar[x][y] = 'W';
+                } else if(map[x][y] < waterThreshold && map[x][y] > groundThreshold){
+                    finalMapAsChar[x][y] = '~';
+                } else if(map[x][y] <= deepWaterThreshold){
+                    finalMapAsChar[x][y] = 'R';
                 } 
             }
         }
 
         WhiteNoiseGenerator wng = WhiteNoiseGenerator.newBuilder().setSeed(seed).build();
+        WhiteNoiseGenerator wng2 = WhiteNoiseGenerator.newBuilder().setSeed(Main.generateSeed()).build();
+
+        WhiteNoiseGenerator[] wngs = {wng, wng2};
 
         float[][] secondProcessMap = new float[bounds[0]][bounds[1]];
 
         for(int x = 0; x < secondProcessMap.length; x++){
             for(int y = 0; y < secondProcessMap[x].length; y++){
-                float xPos = (float) (x * scale);
-                float yPos = (float) (y * scale);
+                float xPos = (float) (x * islandSize);
+                float yPos = (float) (y * islandSize);
 
                 float normalize = 0.0f;
-                secondProcessMap[x][y] += amp * wng.evaluateNoise(xPos * freq + seed, yPos * freq + seed);
+
+                for(WhiteNoiseGenerator w : wngs){
+                    secondProcessMap[x][y] += amp * w.evaluateNoise(xPos * freq + seed, yPos * freq + seed);
+                    normalize += amp;
+                }
+
+                secondProcessMap[x][y] += amp * png.evaluateNoise(xPos * freq + seed, yPos * freq + seed);
                 normalize += amp;
 
                 secondProcessMap[x][y] /= normalize;
 
-                if(secondProcessMap[x][y] <= 0.5f && secondProcessMap[x][y] >= 0.3f && finalMapAsChar[x][y] == 'G' && notSurrounded(finalMapAsChar, x, y)){
+                float treeUpperThreshold = 0.35f;
+                float treeLowerThreshold = 0.2f;
+                if(secondProcessMap[x][y] <= treeUpperThreshold && secondProcessMap[x][y] >= treeLowerThreshold && finalMapAsChar[x][y] == 'G' && notSurrounded(finalMapAsChar, x, y)){
                     finalMapAsChar[x][y] = 'T';
+                } else if(finalMapAsChar[x][y] == 'G' && secondProcessMap[x][y] > treeUpperThreshold){
+                    finalMapAsChar[x][y] = 'g';
                 }
             }
         }
@@ -72,50 +93,55 @@ public class Generation {
         Structures[] structures = Structures.getStructures();
         for(int x = 0; x < terrainMap.length; x++){
             for(int y = 0; y < terrainMap[x].length; y++){
-                for(Structures s : structures){
-                    if(terrainMap[x][y] == 'G'){
-                        if(s.getName().equals("city")){
-                            terrainMap = processCity(terrainMap, x, y);
-                        } else if(x + s.getStructure().length < terrainMap.length && y + s.getStructure()[0].length < terrainMap[x].length){
-                            boolean canPlace = true;
-                            for(int i = 0; i < s.getStructure().length; i++){
-                                for(int j = 0; j < s.getStructure()[i].length; j++){
-                                    if(terrainMap[x + i][y + j] != 'G'){
-                                        canPlace = false;
-                                    }
-                                }
-                            }
-                            if(canPlace){
-                                for(int i = 0; i < s.getStructure().length; i++){
-                                    for(int j = 0; j < s.getStructure()[i].length; j++){
-                                        terrainMap[x + i][y + j] = s.getStructure()[i][j];
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+                Structures s = structures[(int) (Math.random() * structures.length)];
+                processStructures(terrainMap, x, y, s);
             }
         }
 
         return processCharMap(terrainMap);
     }
 
+    private static char[][] processStructures(char[][] terrainMap, int x, int y, Structures s){
+        if((terrainMap[x][y] == 'G' || terrainMap[x][y] == 'g') && Math.random() >= 0.8){
+            if(s.getName().equals("city")){
+                terrainMap = processCity(terrainMap, x, y);
+            } else if(x + s.getStructure().length < terrainMap.length && y + s.getStructure()[0].length < terrainMap[x].length){
+                boolean canPlace = true;
+                for(int i = 0; i < s.getStructure().length; i++){
+                    for(int j = 0; j < s.getStructure()[i].length; j++){
+                        if(terrainMap[x + i][y + j] != 'G'){
+                            canPlace = false;
+                        }
+                    }
+                }
+                if(canPlace){
+                    for(int i = 0; i < s.getStructure().length; i++){
+                        for(int j = 0; j < s.getStructure()[i].length; j++){
+                            terrainMap[x + i][y + j] = s.getStructure()[i][j];
+                        }
+                    }
+                }
+            }
+        }
+        return terrainMap;
+    }
+
     private static char[][] processCity(char[][] map, int x, int y){
-        char[][] fullCityStrucutre = Structures.getFullCity();
-        if(x + fullCityStrucutre.length < map.length && y + fullCityStrucutre[0].length < map[x].length){
+        char[][] fullCityStructure = Structures.getFullCity();
+        if(x + fullCityStructure.length < map.length && y + fullCityStructure[0].length < map[x].length){
             boolean canPlace = true;
-            for(int i = 0; i < fullCityStrucutre.length; i++){
-                for(int j = 0; j < fullCityStrucutre[i].length; j++){
-                    if(map[x + i][y + j] != 'G'){
+            for(int i = 0; i < fullCityStructure.length; i++){
+                for(int j = 0; j < fullCityStructure[i].length; j++){
+                    if(map[x + i][y + j] != 'G' || map[x + i][y + j] != 'g'){
                         canPlace = false;
                     }
                 }
             }
             if(canPlace){
-                for(int i = 0; i < fullCityStrucutre.length; i++){
-                    for(int j = 0; j < fullCityStrucutre[i].length; j++){
-                        map[x + i][y + j] = fullCityStrucutre[i][j];
+                System.out.println("Placing city at (" + x + ", " + y + ")");
+                for(int i = 0; i < fullCityStructure.length; i++){
+                    for(int j = 0; j < fullCityStructure[i].length; j++){
+                        map[x + i][y + j] = fullCityStructure[i][j];
                     }
                 }
             }
@@ -146,19 +172,28 @@ public class Generation {
         boolean right = false;
 
         try{
-            top = map[x][y - 1] == 'G';
-            topL = map[x - 1][y - 1] == 'G';
-            topR = map[x + 1][y - 1] == 'G';
-            bottom = map[x][y + 1] == 'G';
-            bottomL = map[x - 1][y + 1] == 'G';
-            bottomR = map[x + 1][y + 1] == 'G';
-            left = map[x - 1][y] == 'G';
-            right = map[x + 1][y] == 'G';
+            top = map[x][y - 1] == 'G' || map[x][y - 1] == '~';
+            topL = map[x - 1][y - 1] == 'G' || map[x][y - 1] == '~';
+            topR = map[x + 1][y - 1] == 'G' || map[x][y - 1] == '~';
+            bottom = map[x][y + 1] == 'G' || map[x][y - 1] == '~';
+            bottomL = map[x - 1][y + 1] == 'G' || map[x][y - 1] == '~';
+            bottomR = map[x + 1][y + 1] == 'G' || map[x][y - 1] == '~';
+            left = map[x - 1][y] == 'G' || map[x][y - 1] == '~';
+            right = map[x + 1][y] == 'G' || map[x][y - 1] == '~';
         } catch(ArrayIndexOutOfBoundsException e){
             return true;
         }
 
-        if((top && bottom && left && right) || (topL && topR && bottomL && bottomR)){
+        boolean[] bools = {top, topL, topR, bottom, bottomL, bottomR, left, right};
+        int count = 0;
+
+        for(boolean b : bools){
+            if(b && (Math.random() > 0.5)){
+                count++;
+            }
+        }
+
+        if(count >= 5){
             return true;
         }
         return false;
@@ -174,22 +209,35 @@ public class Generation {
                 Color color;
                 switch (map[x][y]) {
                     case 'G':
-                        color = Color.GREEN;
+                        Color[] greens = {Color.GREEN, new Color(0, 200, 0), Color.GREEN, Color.GREEN, new Color(0, 220, 0)};
+                        color = greens[(int) (Math.random() * greens.length)];
+                        break;
+                    case 'g':
+                        Color[] greens2 = {Color.GREEN, Color.GREEN, new Color(0, 200, 0), new Color(0, 220, 0)};
+                        color = greens2[(int) (Math.random() * greens2.length)];
                         break;
                     case 'W':
-                        color = Color.GRAY;
+                        color = Color.DARK_GRAY;
+                        break;
+                    case 'F':
+                        color = new Color(139, 69, 19);
                         break;
                     case 'T':
-                        color = new Color(165, 42, 42);
+                        Color[] browns = {new Color(165, 42, 42), new Color(160, 82, 45)};
+                        color = browns[(int) (Math.random() * browns.length)];
                         break;
                     case '~':
-                        color = Color.BLUE;
+                        Color[] blues = {Color.BLUE, new Color(0, 0, 225), Color.BLUE, Color.BLUE};
+                        color = blues[(int) (Math.random() * blues.length)];
+                        break;
+                    case 'R':
+                        color = new Color(0, 0, 200); // deep water color
                         break;
                     case 'I':
                         color = Color.RED;
                         break;
                     default:
-                        color = Color.BLACK;
+                        color = new Color(85, 21, 21);
                         break;
                 }
                 image.setRGB(x, y, color.getRGB());
--- src/Main.java
+++ src/Main.java
@@ -12,12 +12,12 @@ public class Main{
     public static void main(String[] args){
         File map = new File("./map.md");
         int seed = generateSeed();
-        int[] bounds = {100, 100};
+        int[] bounds = {500, 500};
         try{
             FileWriter fw = new FileWriter(map);
             fw.write("Game Map:\n```\n");
             fw.write(Generation.generateTerrainMap(seed, bounds));
-            fw.write("```\nKey:\n```\nW - Wall\nw - Water\nG - Grass\nT - Tree\nI - Interactable\n```\n\nSeed: " + seed);
+            fw.write("```\nKey:\n```\nW - Wall\nw - Water\nR - Deep Water\nG - Grass\nT - Tree\nI - Interactable\n```\n\nSeed: " + seed);
             fw.close();
         } catch(IOException e){e.printStackTrace();}
     }
